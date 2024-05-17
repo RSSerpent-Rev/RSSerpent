e88dabb6c263635fce66ba6256a5da73129f00a6
EkkoG <beijiu572@gmail.com>
2024-05-17 14:48:37 +0800

    docs: Add comment for plugin

--- rsserpent/plugins/externals.py
+++ rsserpent/plugins/externals.py
@@ -5,6 +5,7 @@ from importlib_metadata import entry_points
 from ..models import Plugin
 
 
+# https://packaging.python.org/en/latest/guides/creating-and-discovering-plugins/#using-package-metadata
 plugins = [cast(Plugin, ep.load()) for ep in entry_points(group="rsserpent.plugin")]
 
 __all__ = ("plugins",)
