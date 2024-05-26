42945dcfe9eeed0349c0165a1066fcf4e5d85d62
EkkoG <beijiu572@gmail.com>
2024-05-26 14:54:19 +0800

    tests: add test for all_routes

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,3 +6,9 @@ def test_index(client: TestClient) -> None:
     response = client.get("/")
     assert response.status_code == 200
     assert "RSSerpent is up & running" in response.text
+
+def test_all_routes(client: TestClient) -> None:
+    """Test if the all_routes routes works properly."""
+    response = client.get("/all_routes")
+    assert response.status_code == 200
+    assert "Plugins Information" in response.text
