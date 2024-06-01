79f69bfc2413f399af1b8a2df3b0c8d955459e3c
EkkoG <beijiu572@gmail.com>
2024-06-01 10:59:39 +0800

    fix: crash when not run with uvicorn

--- rsserpent_rev/utils/log.py
+++ rsserpent_rev/utils/log.py
@@ -1,11 +1,14 @@
 import logging
 import os
 
-from uvicorn.logging import DefaultFormatter
-
 logger = logging.getLogger("rsserpent")
-formatter = DefaultFormatter(fmt="%(levelprefix)s %(message)s")
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+try:
+    from uvicorn.logging import DefaultFormatter
+
+    formatter = DefaultFormatter(fmt="%(levelprefix)s %(message)s")
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+except ImportError:
+    logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 logger.setLevel(os.environ.get("LOG_LEVEL", "INFO"))
