eef44d0f21ca0c442c9f7056d232f50a9854a2ff
EkkoG <beijiu572@gmail.com>
2024-06-03 23:43:14 +0800

    chore: remove feedparser-to-feedgen

--- rsserpent_rev/utils/__init__.py
+++ rsserpent_rev/utils/__init__.py
@@ -1,7 +1,6 @@
 from .cache import cached
-from .feed_parser import to_feedgen
 from .http import Browser, HTTPClient
 from .provider import fetch_data
 from .ratelimit import ratelimit
 
-__all__ = ("Browser", "HTTPClient", "cached", "fetch_data", "ratelimit", "to_feedgen")
+__all__ = ("Browser", "HTTPClient", "cached", "fetch_data", "ratelimit")
